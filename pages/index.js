4f83aa1fe60f20dbff3e76d0beae74d004eb7b4f
Ash Fallon <ashfallon23@gmail.com>
2022-06-08 14:14:28 +0100

    Added fetch request to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
+
 import Card from '../components/Card';
 
 const HomePage = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=27928358-75f1204202f348b034a987023&q=${searchTerm}&image_type=photo&pretty=true`,
+    )
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return <Card />;
 };
 
